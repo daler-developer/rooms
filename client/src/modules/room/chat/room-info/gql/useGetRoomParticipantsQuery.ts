0216502842bc240b1bea27648fcc495428bcb3da
dalersaidov <daler.developer@mail.ru>
2025-05-24 18:30:04 +0500

    CHORES: removed unused code

--- client/src/modules/room/chat/room-info/gql/useGetRoomParticipantsQuery.ts
+++ client/src/modules/room/chat/room-info/gql/useGetRoomParticipantsQuery.ts
@@ -1,58 +1,12 @@
 import { useQuery } from "@apollo/client";
-import { GET_ROOM_PARTICIPANTS_QUERY, PARTICIPANT_JOINED_SUB, PARTICIPANT_LEFT_SUB } from "./tags";
-import { RoomChatGetRoomParticipantsQueryVariables, RoomChatGetRoomParticipantsQuery } from "@/__generated__/graphql";
-import { useEffect } from "react";
+import { GET_ROOM_PARTICIPANTS_QUERY } from "./tags";
+import { RoomChatGetRoomParticipantsQueryVariables } from "@/__generated__/graphql";
 
 const useGetRoomParticipantsQuery = (variables: RoomChatGetRoomParticipantsQueryVariables) => {
-  const query = useQuery(GET_ROOM_PARTICIPANTS_QUERY, {
+  return useQuery(GET_ROOM_PARTICIPANTS_QUERY, {
     notifyOnNetworkStatusChange: true,
     variables,
   });
-
-  useEffect(() => {
-    if (query.data) {
-      // const unsubscribeParticipantJoined = query.subscribeToMore({
-      //   document: PARTICIPANT_JOINED_SUB,
-      //   variables: {
-      //     roomId: variables.id,
-      //   },
-      //   updateQuery(prev, { subscriptionData }): RoomChatGetRoomParticipantsQuery {
-      //     const newParticipant = subscriptionData.data.roomParticipantJoined;
-      //     return {
-      //       ...prev,
-      //       room: {
-      //         ...prev.room,
-      //         participants: [...prev.room.participants, newParticipant],
-      //       },
-      //     };
-      //   },
-      // });
-
-      // const unsubscribeParticipantLeft = query.subscribeToMore({
-      //   document: PARTICIPANT_LEFT_SUB,
-      //   variables: {
-      //     roomId: variables.id,
-      //   },
-      //   updateQuery(prev, { subscriptionData }): RoomChatGetRoomParticipantsQuery {
-      //     const participant = subscriptionData.data.roomParticipantLeft;
-      //     return {
-      //       ...prev,
-      //       room: {
-      //         ...prev.room,
-      //         participants: prev.room.participants.filter((_participant) => _participant.id !== participant.id),
-      //       },
-      //     };
-      //   },
-      // });
-
-      return () => {
-        // unsubscribeParticipantJoined();
-        // unsubscribeParticipantLeft();
-      };
-    }
-  }, [query.data]);
-
-  return query;
 };
 
 export default useGetRoomParticipantsQuery;
